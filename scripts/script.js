ac023142bf3945a9f0f2265b36c5246d17d46cea
conor <doyle.conorp@gmail.com>
2022-03-03 14:29:47 -0800

    Link script to index by occupation title array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 let sampleOccupationTitles = ['Marketing Coordinator', 'Medical Assistant', 'Web Designer', 'Nursing Assistant', 'Librarian']
+
+let sel = document.getElementById('jobSelect')
+for (let i = 0; i < sampleOccupationTitles.length; i++ ) {
+    let opt = document.createElement('option');
+    opt.innerHTML = sampleOccupationTitles[i];
+    opt.value = sampleOccupationTitles[i];
+    sel.appendChild(opt);
+}
